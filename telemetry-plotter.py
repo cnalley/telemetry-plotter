1341880bad001e5bef88e8ab9bb0f7763ef906ac
cnalley <cnalley@kubos.co>
2017-02-19 12:09:54 -0800

    Ran through lint check

--- telemetry-plotter.py
+++ telemetry-plotter.py
@@ -1,33 +1,37 @@
 import os
-import sys
 import binascii
 import ctypes
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import enum
-import argparse
 
 
 class TELEMETRY_DATA_TYPE(enum.Enum):
     TELEMETRY_TYPE_INT = 0
     TELEMETRY_TYPE_FLOAT = 1
 
+
 class TELEMETRY_UNION(ctypes.Union):
     _fields_ = [("i", ctypes.c_int),
                 ("f", ctypes.c_float)]
+
 
 class TELEMETRY_SOURCE(ctypes.Structure):
     _fields_ = [("topic_id", ctypes.c_uint8),
                 ("telemetry_data_type", ctypes.c_int),
                 ("subsystem_id", ctypes.c_uint8)]
-                
+
+
 class TELEMETRY_PACKET(ctypes.Structure):
     _fields_ = [("source", TELEMETRY_SOURCE),
                 ("data", TELEMETRY_UNION),
-                ("timestamp", ctypes.c_uint16)] 
+                ("timestamp", ctypes.c_uint16)]
+
     def __init__(self, bytes):
         fit = min(len(bytes), ctypes.sizeof(self))
         ctypes.memmove(ctypes.addressof(self), bytes, fit)
+
 
 class telemetry_plotter(object):
     def __init__(self, x_values, y_values, plot_name, series_name):
@@ -35,19 +39,19 @@ class telemetry_plotter(object):
         self.y_values = y_values
         self.plot_name = plot_name
         self.series_name = series_name
-    
+
     def plot(self):
-        plt.plot(np.asarray(self.x_values),           # X Axis
-                        np.asarray(self.y_values),    # Y Axis
-                        'o', label=self.series_name,    # Circle marker and legend
-                        picker=5)                     # 5 points tolerance
-    
+        plt.plot(np.asarray(self.x_values),    # X Axis
+                 np.asarray(self.y_values),    # Y Axis
+                 'o', label=self.series_name,  # Circle marker and legend
+                 picker=5)                     # 5 points tolerance
+
         plt.ylabel('Value', picker=True)
-        plt.xlabel('Time',  picker=True)
-        plt.gcf().autofmt_xdate()           # Beautify time x axis
+        plt.xlabel('Time', picker=True)
+        plt.gcf().autofmt_xdate()              # Beautify time x axis
         plt.grid(True)
         plt.title(self.plot_name)
-        plt.legend(bbox_to_anchor=(1, -0.2),loc=5, borderaxespad=0)
+        plt.legend(bbox_to_anchor=(1, -0.2), loc=5, borderaxespad=0)
         plt.show()
 
 
@@ -56,15 +60,15 @@ class get_telemetry(object):
 
         self.x_value_list = []
         self.y_value_list = []
-        
+
         if not os.path.exists(file_path):
-            print('Log file not found')
+            print 'Log file not found'
             return
 
         try:
             telemetry_log = open(file_path, "r")
         except IOError:
-            print('There was an error opening the log file')
+            print 'There was an error opening the log file'
             return
 
         for line in iter(telemetry_log):
@@ -72,15 +76,16 @@ class get_telemetry(object):
             packed_data = binascii.unhexlify(packet[2])
             telemetry_packet = TELEMETRY_PACKET(packed_data)
             self.x_value_list.append(telemetry_packet.timestamp)
-            
+
             if telemetry_packet.source.telemetry_data_type == TELEMETRY_DATA_TYPE.TELEMETRY_TYPE_INT.value:
                 self.y_value_list.append(telemetry_packet.data.i)
             if telemetry_packet.source.telemetry_data_type == TELEMETRY_DATA_TYPE.TELEMETRY_TYPE_FLOAT.value:
                 self.y_value_list.append(telemetry_packet.data.f)
             else:
-                print('Telemetry type not found') 
+                print 'Telemetry type not found'
 
         telemetry_log.close()
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -88,9 +93,10 @@ def main():
     args = parser.parse_args()
 
     telemetry = get_telemetry(args.filepath)
-    
+
     plotter = telemetry_plotter(telemetry.x_value_list, telemetry.y_value_list, 'Telemetry Plotter', 'Series Data')
     plotter.plot()
+
 
 if __name__ == '__main__':
     main()
